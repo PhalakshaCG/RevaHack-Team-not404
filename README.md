111e1604502cb38e9d8dedcdce7eba511eb7d5d3
The-Maestro-JJ <jerilsabanjoy.cs19@rvce.edu.in>
2022-11-12 17:32:17 +0530

    rebase

--- README.md
+++
@@ -1,2 +0,0 @@
-# RevaHack-Team-not404
-dApp for Reva Hackathon by Team !404 using polygon blockchain
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPost from "../components/NewPost";
 import "../static/Common.css";
 import Post from "../components/Post";
+import ConfirmPost from "../components/ConfirmPost";
+import Maximized from "../components/Maximized";
 
 function Home() {
   let posts = [
@@ -20,7 +22,7 @@ function Home() {
           name: "tag1",
         },
         {
-          id: 2,
+          id: 3,
           name: "tag1",
         },
       ],
@@ -61,27 +63,60 @@ function Home() {
       ],
     },
   ];
-  return (
-    <>
-      <div className="px-[4vw]">
-        <NewPost />
-      </div>
-      <hr />
-      <div className="posts mt-5 px-[4vw]">
-        {posts.map((post) => {
-          return (
-            <div key={posts.id} className="post">
+  const [confirmPost, setConfirmPost] = useState(false);
+  const [maximizedPost, setMaximizedPost] = useState(false);
+  const [PostData, setPostData] = useState({});
+  if (!confirmPost && !maximizedPost) {
+    return (
+      <div>
+        <div className="px-[4vw]">
+          <NewPost
+            setConfirmPost={setConfirmPost}
+            setMaximizedPost={setMaximizedPost}
+            PostData={PostData}
+            setPostData={setPostData}
+          />
+        </div>
+        <hr />
+        <div className="posts mt-5 px-[4vw]">
+          {posts.map((post) => {
+            return (
               <Post
+                key={post.id}
                 title={post.title}
                 description={post.description}
                 tags={post.tags}
               />
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        {confirmPost ? (
+          <div className="px-[4vw]">
+            <ConfirmPost
+              setConfirmPost={setConfirmPost}
+              setMaximizedPost={setMaximizedPost}
+              PostData={PostData}
+              setPostData={setPostData}
+            />
+          </div>
+        ) : (
+          <div className="px-[4vw]">
+            <Maximized
+              setConfirmPost={setConfirmPost}
+              setMaximizedPost={setMaximizedPost}
+              PostData={PostData}
+              setPostData={setPostData}
+            />
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
 export default Home;
